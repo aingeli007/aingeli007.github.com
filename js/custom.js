031f01e10292aa0746dd450b6061a49b11d4f4fb
Aingeli Anton <96458583+aingeli007@users.noreply.github.com>
2022-04-05 18:35:40 -0400

    Added case study.

--- js/custom.js
+++ js/custom.js
@@ -14,50 +14,13 @@
 
   $(window).scroll(function() {
         if ($(this).scrollTop() > 200) {
-        $('.go-top1').fadeIn(200);
+        $('.go-top').fadeIn(200);
         } else {
-          $('.go-top1').fadeOut(200);
+          $('.go-top').fadeOut(200);
         }
         });
         // Animate the scroll to top
-      $('.go-top1').click(function(event) {
+      $('.go-top').click(function(event) {
         event.preventDefault();
       $('html, body').animate({scrollTop: 0}, 300);
       })
-  $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-        $('.go-top2').fadeIn(200);
-        } else {
-          $('.go-top2').fadeOut(200);
-        }
-        });
-          // Animate the scroll to top
-        $('.go-top2').click(function(event) {
-          event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 300);
-        })
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-        $('.go-top3').fadeIn(200);
-        } else {
-          $('.go-top3').fadeOut(200);
-        }
-        });
-          // Animate the scroll to top
-          $('.go-top3').click(function(event) {
-            event.preventDefault();
-          $('html, body').animate({scrollTop: 0}, 300);
-          })
-    $(window).scroll(function() {
-          if ($(this).scrollTop() > 200) {
-          $('.go-top4').fadeIn(200);
-          } else {
-            $('.go-top4').fadeOut(200);
-          }
-          });
-            // Animate the scroll to top
-          $('.go-top4').click(function(event) {
-            event.preventDefault();
-          $('html, body').animate({scrollTop: 0}, 300);
-          })
-    }
